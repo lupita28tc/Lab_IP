fca6de969a9af0cace400e3a452f646cf0aa939f
Miriam Trejo <lupitatrejo28tc@gmail.com>
2026-08-27 12:47:07 -0500

    CREATE PrimoFibonacci.py en Unidad1_Practica_Final

---
+++ Unidad1_Practica_Final/PrimoFibonacci.py
@@ -0,0 +1,27 @@
+numero=int(input("Leer número"))
+es_primo=True 
+
+if numero <= 1:
+    es_primo=False 
+else:
+    i=2
+    while i<numero:
+      if numero% i ==0:
+        es_primo= False 
+        break
+      i=i+1 
+        
+if es_primo == True:
+  print("ES primo")
+  a=0
+  b=1 
+  while a < numero:
+    siguiente = a + b
+    a=b
+    b=siguiente
+  if a == numero:
+    print("Esta en Fibonacci")
+  else:
+     print("No esta en Fibonacci")
+else:
+    print("No es primo")
