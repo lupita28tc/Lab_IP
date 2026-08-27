02cb92bdaf1ecd31fb76dc588a4969cf2bf39115
Miriam Trejo <lupitatrejo28tc@gmail.com>
2026-08-27 13:19:13 -0500

    CREATE Tipos.py en 01Ejercicio

--- 01Ejercicio/Tipos.py
+++ 01Ejercicio/Tipos.py
@@ -5,4 +5,34 @@ x=10.0
 type(x)
 print(type(x))
 
+x="10"
+print (type(x))
 
+y=int(x)
+print (type(y))
+print(y)
+
+x="20"
+y="30"
+z=x + y
+print(z)
+
+x=20
+y=30.0
+z=x+y
+print(z)
+
+x=20
+y=30
+z=x/y
+print(z)
+
+x=30
+y=30
+z=x*z
+print(z)
+
+x=30
+y=30.0
+z=x*y
+print(z)
