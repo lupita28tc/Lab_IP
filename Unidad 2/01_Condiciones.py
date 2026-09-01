16b71e4dd7c6068bc7d117224318512a38c45b5e
Miriam Trejo <lupitatrejo28tc@gmail.com>
2026-09-01 16:02:13 -0500

    CREATE Unidad 2

---
+++ Unidad 2/01_Condiciones.py
@@ -0,0 +1,11 @@
+print (bool(1))
+
+print (bool(0))
+
+print (bool(-1))
+
+print (bool(10000))
+
+print (bool(10.5))
+
+print (bool("A"))  #Todo valor diferente a 0 dara valor TRUE 
---
+++ Unidad 2/02_Operadores.py
@@ -0,0 +1,6 @@
+operador= input ("inserta un operador(+,-,*,/): ")
+if operador == "+":
+  O1=input ("ingrese un numero")
+  O2=input ("ingrese un numero")
+  resultado=O1+operador+O2
+  print(eval(resultado))
