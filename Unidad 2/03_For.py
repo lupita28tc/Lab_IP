8374934e5c241a73e8b099cff6a63bd30d9d028c
Miriam Trejo <lupitatrejo28tc@gmail.com>
2026-09-03 12:55:23 -0500

    CREATE unidad 2

---
+++ Unidad 2/03_For.py
@@ -0,0 +1,18 @@
+#Trabajando con rangos en for 
+for numero in range(0,7,3):
+    cuadrado = numero ** 2
+    print (numero,cuadrado)
+    
+#trabajando con listas en for     
+materias=["Python","Linux","Interfaces"]
+for materia in materias:
+    print (materia)
+    
+cadena ="0123456789ABCDEF"
+for letra in cadena:
+    print(letra)
+     
+for i in range(len(cadena)):
+    print(cadena[i])
+    
+    
---
+++ Unidad 2/04_For 1_100.py
@@ -0,0 +1,3 @@
+
+for numero in range(0,101,2): 
+    print (numero )
